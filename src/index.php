a3a97b0f33c9de435360c4a12c1689613ae3b5ec
Rômulo Guimarães <romulo1984@gmail.com>
2018-04-25 17:24:54 -0300

    Multiple Urls

--- src/index.php
+++ src/index.php
@@ -8,24 +8,33 @@ include __DIR__ . '/libs/GoogleAnalyticsRealTime.php';
 
 $app = new \Slim\App;
 
+$app->get('/realtime/batch/{urls:.+}', function (Request $request, Response $response, array $args) {
+    $realTime = new GoogleAnalyticsRealTime();
+
+    $items = explode(';', $args['urls']);
+    $finalParsedUrls = [];
+
+    foreach($items as $value) {
+        $item = explode(',', $value);
+        $finalParsedUrls[$item[0]] = $item[1];
+    }
+
+    $response = $response->withJson(['data' => $realTime->getBatch($finalParsedUrls)]);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Methods', 'GET');
+});
+
 // $app->get('/realtime[/{url:.+}]', function (Request $request, Response $response, array $args) {
+//     var_dump($args['url']);
 //     $realTime = new GoogleAnalyticsRealTime();
-//     $realTime->setParams('filters', 'rt:pagePath=@/' . ($args['url'] ?? ''));
+//     $realTime->setParams('filters', 'rt:pagePath=@' . ($args['url'] ?? ''));
 
 //     $response = $response->withJson(['data' => $realTime->getActiveUsers()]);
 //     return $response
 //         ->withHeader('Access-Control-Allow-Origin', '*')
 //         ->withHeader('Access-Control-Allow-Methods', 'GET');
 // });
-
-$app->get('/realtime/batch', function (Request $request, Response $response) {
-    $realTime = new GoogleAnalyticsRealTime();
-
-    $response = $response->withJson(['data' => $realTime->getBatch()]);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
-        ->withHeader('Access-Control-Allow-Methods', 'GET');
-});
 
 $app->run();
 
